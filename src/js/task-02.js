f7b491a02ceebd0a3f576c2a53bfdcf601a4fd4c
anya.shan <anya.shanaida@yahoo.com>
2022-07-23 12:53:53 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listIngredientsEl = document.querySelector('#ingredients');
+
+const itemsIngredientsEl = ingredients.map(ingredient => {
+  const itemIngredientsEl = document.createElement('li');
+  itemIngredientsEl.classList.add('item');
+  itemIngredientsEl.textContent = `${ingredient}`;
+  return itemIngredientsEl;
+});
+
+listIngredientsEl.append(...itemsIngredientsEl);
