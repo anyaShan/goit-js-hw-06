938ac706c1c63493c8d705369dcaceb0d39325b4
anya.shan <anya.shanaida@yahoo.com>
2022-07-25 17:41:39 +0300

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const formDataEl = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formDataEl);
+  event.currentTarget.reset();
+}
